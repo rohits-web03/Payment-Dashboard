92411ec298463110f6bd3e528a2b1ce7c4c3b08c
rohits-web03 <rohit.dey302003@gmail.com>
2024-01-09 05:48:13 +0530

    Made the OverviewCard

--- src/components/Payments/OverviewCard.jsx
+++ src/components/Payments/OverviewCard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const OverviewCard = () => {
+const OverviewCard = ({title,value,money}) => {
   return (
-    <div>OverviewCard</div>
+    <div className='flex flex-col justify-center items-start bg-[#ffffff] rounded-lg m-2 p-6 shadow'>
+      <p className='text-slate-900 text-lg'>{title}</p>
+      <p className='font-semibold text-3xl'>{money?'₹':''}{value}</p>
+    </div>
   )
 }
 
