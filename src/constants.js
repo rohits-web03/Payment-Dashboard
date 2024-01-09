4707593b76509b19967930e2b70ffceb5ea40775
rohits-web03 <rohit.dey302003@gmail.com>
2024-01-09 05:46:19 +0530

    Added icons to sidebarLinks and added random transactionData

--- src/constants.js
+++ src/constants.js
@@ -1,49 +1,134 @@
+import { MdOutlineHome } from "react-icons/md";
+import { PiNotepadBold } from "react-icons/pi";
+import { IoGridOutline } from "react-icons/io5";
+import { LuTruck } from "react-icons/lu";
+import { PiSpeakerHighBold } from "react-icons/pi";
+import { TbBrandGoogleAnalytics } from "react-icons/tb";
+import { MdPayments } from "react-icons/md";
+import { SlCursor } from "react-icons/sl";
+import { LuBadgePercent } from "react-icons/lu";
+import { PiUsersBold } from "react-icons/pi";
+import { IoColorPaletteOutline } from "react-icons/io5";
+import { AiOutlineThunderbolt } from "react-icons/ai";
+
 export const sidebarLinks=[
     {
         label:'Home',
-        href:'#'
+        href:'#',
+        icon:MdOutlineHome
     },
     {
         label:'Orders',
-        href:'#'
+        href:'#',
+        icon:PiNotepadBold
     },    
     {
         label:'Products',
-        href:'#'
+        href:'#',
+        icon:IoGridOutline
     },    
     {
         label:'Delivery',
-        href:'#'
+        href:'#',
+        icon:LuTruck
     },    
     {
         label:'Marketing',
-        href:'#'
-    },    {
+        href:'#',
+        icon:PiSpeakerHighBold
+    },    
+    {
         label:'Analytics',
-        href:'#'
+        href:'#',
+        icon:TbBrandGoogleAnalytics
     },    
     {
         label:'Payments',
-        href:'#'
+        href:'#',
+        icon:MdPayments
     },    
     {
         label:'Tools',
-        href:'#'
+        href:'#',
+        icon:SlCursor
     },    
     {
         label:'Discounts',
-        href:'#'
+        href:'#',
+        icon:LuBadgePercent
     },    
     {
         label:'Audience',
-        href:'#'
+        href:'#',
+        icon:PiUsersBold
     },    
     {
         label:'Appearance',
-        href:'#'
+        href:'#',
+        icon:IoColorPaletteOutline
     },
     {
         label:'Plugins',
-        href:'#'
+        href:'#',
+        icon:AiOutlineThunderbolt
     },
+];
+
+export const transactionData=[
+    {
+        orderId:'#281209',
+        orderDate:'14.5.2023',
+        orderAmount:'₹1,278.23',
+        transactionFees:'₹9945.68'
+    },
+    {"orderId":"#281209","orderDate":"27.7.2023","orderAmount":"₹6785.62","transactionFees":"₹8316.15"},
+    {"orderId":"#281209","orderDate":"4.7.2023","orderAmount":"₹4266.40","transactionFees":"₹2149.08"},
+    {"orderId":"#281209","orderDate":"25.1.2023","orderAmount":"₹5413.15","transactionFees":"₹1536.66"},
+    {"orderId":"#281209","orderDate":"27.5.2023","orderAmount":"₹7207.15","transactionFees":"₹645.92"},
+    {"orderId":"#281209","orderDate":"22.7.2023","orderAmount":"₹9438.38","transactionFees":"₹3064.16"},
+    {"orderId":"#281209","orderDate":"1.9.2023","orderAmount":"₹2111.23","transactionFees":"₹9630.68"},
+    {"orderId":"#281209","orderDate":"26.3.2023","orderAmount":"₹5560.68","transactionFees":"₹6830.77"},
+    {"orderId":"#281209","orderDate":"13.10.2023","orderAmount":"₹3643.27","transactionFees":"₹9062.92"},
+    {"orderId":"#281209","orderDate":"31.7.2023","orderAmount":"₹1150.19","transactionFees":"₹5159.22"},
+    {"orderId":"#281209","orderDate":"14.5.2023","orderAmount":"₹1849.70","transactionFees":"₹5969.78"},
+    {"orderId":"#281209","orderDate":"17.6.2023","orderAmount":"₹1418.20","transactionFees":"₹3295.80"},
+    {"orderId":"#281209","orderDate":"20.7.2023","orderAmount":"₹2117.38","transactionFees":"₹3570.56"},
+    {"orderId":"#281209","orderDate":"18.11.2023","orderAmount":"₹5545.58","transactionFees":"₹3362.99"},
+    {"orderId":"#281209","orderDate":"16.2.2023","orderAmount":"₹0.11","transactionFees":"₹2193.66"},
+    {"orderId":"#281209","orderDate":"20.4.2023","orderAmount":"₹9022.21","transactionFees":"₹4561.59"},
+    {"orderId":"#281209","orderDate":"23.6.2023","orderAmount":"₹2675.19","transactionFees":"₹7530.08"},
+    {"orderId":"#281209","orderDate":"8.3.2023","orderAmount":"₹7989.51","transactionFees":"₹8324.58"},
+    {"orderId":"#281209","orderDate":"31.10.2023","orderAmount":"₹8799.15","transactionFees":"₹9583.03"},
+    {"orderId":"#281209","orderDate":"31.8.2023","orderAmount":"₹6696.57","transactionFees":"₹6235.52"},
+    {"orderId":"#281209","orderDate":"31.7.2023","orderAmount":"₹548.00","transactionFees":"₹882.00"},
+    {"orderId":"#281209","orderDate":"31.8.2023","orderAmount":"₹7967.00","transactionFees":"₹6712.18"},
+    {"orderId":"#281209","orderDate":"23.1.2023","orderAmount":"₹8095.48","transactionFees":"₹5958.49"},
+    {"orderId":"#281209","orderDate":"7.11.2023","orderAmount":"₹5226.56","transactionFees":"₹8203.01"},
+    {"orderId":"#281209","orderDate":"22.2.2023","orderAmount":"₹2373.22","transactionFees":"₹4429.41"},
+    {"orderId":"#281209","orderDate":"31.7.2023","orderAmount":"₹2570.86","transactionFees":"₹3443.77"},
+    {"orderId":"#281209","orderDate":"13.8.2023","orderAmount":"₹9802.33","transactionFees":"₹5691.41"},
+    {"orderId":"#281209","orderDate":"14.5.2023","orderAmount":"₹1049.41","transactionFees":"₹8052.96"},
+    {"orderId":"#281209","orderDate":"8.7.2023","orderAmount":"₹5737.38","transactionFees":"₹8941.93"},
+    {"orderId":"#281209","orderDate":"3.5.2023","orderAmount":"₹7109.95","transactionFees":"₹4829.24"},
+    {"orderId":"#281209","orderDate":"22.5.2023","orderAmount":"₹5560.45","transactionFees":"₹4673.66"},
+    {"orderId":"#281209","orderDate":"20.9.2023","orderAmount":"₹5274.52","transactionFees":"₹8377.68"},
+    {"orderId":"#281209","orderDate":"5.7.2023","orderAmount":"₹9557.94","transactionFees":"₹3877.09"},
+    {"orderId":"#281209","orderDate":"11.2.2023","orderAmount":"₹9383.73","transactionFees":"₹5333.27"},
+    {"orderId":"#281209","orderDate":"8.1.2024","orderAmount":"₹5773.46","transactionFees":"₹8227.55"},
+    {"orderId":"#281209","orderDate":"29.1.2023","orderAmount":"₹1937.91","transactionFees":"₹6764.86"},
+    {"orderId":"#281209","orderDate":"23.5.2023","orderAmount":"₹9369.84","transactionFees":"₹1432.26"},
+    {"orderId":"#281209","orderDate":"16.2.2023","orderAmount":"₹3190.58","transactionFees":"₹998.27"},
+    {"orderId":"#281209","orderDate":"14.8.2023","orderAmount":"₹5197.03","transactionFees":"₹71.19"},
+    {"orderId":"#281209","orderDate":"6.1.2024","orderAmount":"₹6940.72","transactionFees":"₹7786.31"},
+    {"orderId":"#281209","orderDate":"2.7.2023","orderAmount":"₹1603.38","transactionFees":"₹6760.70"},
+    {"orderId":"#281209","orderDate":"2.6.2023","orderAmount":"₹5459.64","transactionFees":"₹8781.97"},
+    {"orderId":"#281209","orderDate":"23.1.2023","orderAmount":"₹1147.78","transactionFees":"₹1304.05"},
+    {"orderId":"#281209","orderDate":"20.1.2023","orderAmount":"₹4721.19","transactionFees":"₹6548.09"},
+    {"orderId":"#281209","orderDate":"14.7.2023","orderAmount":"₹4530.37","transactionFees":"₹574.26"},
+    {"orderId":"#281209","orderDate":"1.5.2023","orderAmount":"₹5604.70","transactionFees":"₹120.90"},
+    {"orderId":"#281209","orderDate":"21.3.2023","orderAmount":"₹5501.19","transactionFees":"₹4272.59"},
+    {"orderId":"#281209","orderDate":"3.7.2023","orderAmount":"₹4172.47","transactionFees":"₹2865.60"},
+    {"orderId":"#281209","orderDate":"25.2.2023","orderAmount":"₹4352.76","transactionFees":"₹1678.55"},
+    {"orderId":"#281209","orderDate":"1.12.2023","orderAmount":"₹2076.28","transactionFees":"₹2434.93"},
+    {"orderId":"#281209","orderDate":"3.8.2023","orderAmount":"₹6382.93","transactionFees":"₹3965.04"}
 ]
